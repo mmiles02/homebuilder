31877acf70c065d254d3228b228d556a5bee3e36
Mitchell Miles <mitchell.miles02@gmail.com>
2022-05-16 16:29:23 -0400

    vue changes

---
+++ src/pages/Home/Home.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-row>
+    <v-col>
+      <v-row>
+        <v-col>
+          <Header/>
+        </v-col>
+      </v-row>
+
+      <v-row>
+        <v-col>
+          <v-text-field
+            id="password-field"
+            autofocus
+            outlined
+            :type="show ? 'text' : 'password'"
+            @click:append="show = !show"
+            v-model="password"
+            :append-icon="show ? 'mdi-eye' : 'mdi-eye-off'"
+          ></v-text-field>
+        </v-col>
+      </v-row>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: 'HomePage',
+
+  data() {
+    return {
+      show: false,
+      password: ''
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+</style>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,16 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+module.exports = {
+  pages: {
+    'index': {
+      entry: './src/pages/Home/main.js',
+      template: 'public/index.html',
+      title: 'Home',
+      chunks: [ 'chunk-vendors', 'chunk-common', 'index' ]
+    },
+    'builder': {
+      entry: './src/pages/Builder/main.js',
+      template: 'public/index.html',
+      title: 'Builder',
+      chunks: [ 'chunk-vendors', 'chunk-common', 'builder' ]
+    }
+  }
+}
